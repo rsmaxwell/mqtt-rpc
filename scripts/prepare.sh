d0f9b98be0a4dfff5d53f1112afa33f8d06d1c12
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-02-06 11:23:53 +0000

    set FAMILY and ARCH in Jenkinsfile

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,14 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
+set -e
 set -x 
+
+# Check required env variables are set
+case "$FAMILY" in
+  alpine|linux) ;;
+  *)
+    echo "ERROR: invalid FAMILY='$FAMILY' (allowed: alpine|linux)" >&2
+    exit 2
+    ;;
+esac
+
+case "$ARCHITECTURE" in
+  amd64|arm64) ;;
+  *)
+    echo "ERROR: invalid ARCHITECTURE='$ARCHITECTURE' (allowed: amd64|arm64)" >&2
+    exit 2
+    ;;
+esac
+
+
 
 if [ -z "${BUILD_ID}" ]; then
     BUILD_ID="(none)"
     VERSION="0.0.1-SNAPSHOT"
     REPOSITORY=snapshots
+    REPOSITORYID=snapshots
 else
     VERSION="0.0.1.$((${BUILD_ID}))"
     REPOSITORY=releases
+    REPOSITORYID=releases
 fi
 
 
