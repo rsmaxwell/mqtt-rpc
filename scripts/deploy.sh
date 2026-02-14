8d35b535f4e294488b78f6df5aa9565025042119
Richard Maxwell <richard@rsmaxwell.co.uk>
2026-02-14 11:55:54 +0000

    restore export GRADLE_USER_HOME in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,14 +10,9 @@ BUILD_DIR=${PROJECT_DIR}/build
 
 cd ${PROJECT_DIR}
 
-# export GRADLE_USER_HOME=/home/gradle/.gradle
 
-echo "whoami: $(whoami)"
+
 echo "GRADLE_USER_HOME=$GRADLE_USER_HOME"
-echo "HOME=$HOME"
-env | sort | grep -E 'GRADLE|HOME'
-ls -al "$GRADLE_USER_HOME" || true
-ls -al "$GRADLE_USER_HOME/gradle.properties" || true
 
 ${PROJECT_DIR}/gradlew publish --info --stacktrace \
     -PrepositoryName=${REPOSITORY} \
